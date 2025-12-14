ce4a78b0e4898d623a86872a3b261c2ee97f4aea
Jack <jeyden94@gmail.com>
2025-12-13 21:59:30 -0600

    Update - 05ad2522

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const {createServer} = require('node:http');
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello, world!</h1>');
+});
+
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server running at https://localhost:3000')
+});
