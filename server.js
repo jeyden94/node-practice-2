7ca635a9c54e4242346e0c3e3d6fa2a82546342b
Jack <jeyden94@gmail.com>
2025-12-13 22:01:54 -0600

    Update - 76a9140f

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const {createServer} = require('node:http');
 
 const server = createServer((req, res) => {
-    res.statusCode = 200;
+    res.statusCode = 404;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Hello, world!</h1>');
 });
